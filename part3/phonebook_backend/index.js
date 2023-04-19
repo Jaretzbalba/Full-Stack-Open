f8f6c3b89e77d6d8400477e32c1addd4cc2ce769
Jaret Balba <Jaretzbalba@gmail.com>
2023-04-19 14:36:13 -0700

    add middleware to handle static build file

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -9,6 +9,7 @@ morgan.token('data', function getData(req) {
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data')
 );
