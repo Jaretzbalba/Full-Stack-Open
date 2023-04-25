963fdb16773589ca9bcc36891d31802e1c117be0
Jaret Balba <Jaretzbalba@gmail.com>
2023-04-24 17:16:00 -0700

    add linter

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,78 +1,78 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const app = express();
-const Contact = require('./models/contact');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+const Contact = require('./models/contact')
 
 morgan.token('data', function getData(req) {
-  return JSON.stringify(req.body);
-});
+  return JSON.stringify(req.body)
+})
 
-app.use(cors());
-app.use(express.static('build'));
-app.use(express.json());
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data')
-);
+)
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
+  response.send('<h1>Hello World!</h1>')
+})
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   Contact.find({})
     .then(contacts => {
       response.send(
         `<h1>Phonebook has info for ${
           contacts.length
         } people</h1> <p>${new Date().toLocaleString()}</p>`
-      );
+      )
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.get('/api/persons', (request, response) => {
   Contact.find({}).then(contacts => {
-    response.json(contacts);
-  });
-});
+    response.json(contacts)
+  })
+})
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Contact.findById(request.params.id)
     .then(contact => {
       if (contact) {
-        response.json(contact);
+        response.json(contact)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   const person = new Contact({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then(savedContact => {
-      response.json(savedContact);
+      response.json(savedContact)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   const note = {
     name: body.name,
     number: body.number,
-  };
+  }
 
   Contact.findByIdAndUpdate(request.params.id, note, {
     new: true,
@@ -80,42 +80,43 @@ app.put('/api/persons/:id', (request, response, next) => {
     context: 'query',
   })
     .then(updatedContact => {
-      response.json(updatedContact);
+      response.json(updatedContact)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Contact.findByIdAndRemove(request.params.id)
     .then(result => {
-      response.status(204).end();
+      console.log(result)
+      response.status(204).end()
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 // handler of requests with unknown endpoint
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 // this has to be the last loaded middleware.
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
