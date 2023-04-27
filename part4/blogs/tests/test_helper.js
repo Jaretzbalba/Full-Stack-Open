4f7ce6df60f26c75c173ea02a40700c6fe5a1ade
Jaret Balba <Jaretzbalba@gmail.com>
2023-04-26 22:12:58 -0700

    4.17 add username/password validation and user/blog references

--- part4/blogs/tests/test_helper.js
+++ part4/blogs/tests/test_helper.js
@@ -1,5 +1,5 @@
 const Blog = require('../models/blog')
-// const User = require('../models/user')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -58,14 +58,14 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
-// const usersInDb = async () => {
-//   const users = await User.find({})
-//   return users.map(user => user.toJSON())
-// }
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
 
 module.exports = {
   initialBlogs,
   nonExistingId,
   blogsInDb,
-  //   usersInDb,
+  usersInDb,
 }
