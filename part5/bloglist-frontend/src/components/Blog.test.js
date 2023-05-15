773a6f9c08313656c505100e9c4cb47bac9fd90f
Jaret Balba <Jaretzbalba@gmail.com>
2023-05-15 15:17:40 -0700

    5.14 show url/likes when show button is clicked

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-describe('Blog', () => {
+describe('<Blog />', () => {
   const blog = {
     id: '64040f7471b8e66910889a4d',
     title: 'test title',
@@ -17,15 +18,32 @@ describe('Blog', () => {
     },
   }
 
-  let component = render(
-    <Blog
-      blog={blog}
-      user={blog.user}
-    />
-  )
+  const mockLikesHandler = jest.fn()
+  const mockRemoveHandler = jest.fn()
+  let component
+
+  beforeEach(() => {
+    component = render(
+      <Blog
+        blog={blog}
+        likesUpdate={mockLikesHandler}
+        removeBlog={mockRemoveHandler}
+        user={blog.user}
+      />
+    )
+  })
 
   test('only shows title/author at start', () => {
     const div = component.container.querySelector('.detailInfo')
     expect(div).toHaveStyle('display: none')
+  })
+
+  test('shows url/likes after show button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = component.container.querySelector('.viewButton')
+    await user.click(button)
+
+    const div = component.container.querySelector('.detailInfo')
+    expect(div).not.toHaveStyle('display: none')
   })
 })
