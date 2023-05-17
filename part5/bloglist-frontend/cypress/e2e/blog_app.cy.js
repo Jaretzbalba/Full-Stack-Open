de6eef9a3fa91cf93b4f4e2124c48fab9d865fbd
Jaret Balba <Jaretzbalba@gmail.com>
2023-05-17 14:45:56 -0700

    5.18 test successful and unsuccessful logins

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,39 @@
 describe('Blog app', function () {
   beforeEach(function () {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      name: 'Jaret Balba',
+      username: 'Jaret',
+      password: 'password',
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+    cy.visit('')
   })
 
   it('Login form is shown', function () {
     cy.contains('Login to application')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('Jaret')
+      cy.get('#password').type('password')
+      cy.get('#login-button').click()
+
+      cy.get('.sucess').should(
+        'contain',
+        'Jaret Balba was successfully logged in'
+      )
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('Jaret')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
